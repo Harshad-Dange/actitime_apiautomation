55faef6a6e5d0b401959e03c71190bd581242615
Harshad <118425148+harshadlifesize01@users.noreply.github.com>
2023-05-25 10:53:53 +0530

    automated get customer by id api

--- src/test/Features/Customer.feature
+++ src/test/Features/Customer.feature
@@ -1,7 +1,16 @@
+@GetAllCustomer
 Feature: Verify customer feature
 
-  @GetAllCustomer
   Scenario: Verify all customer information in get all customer api response
     Given I set up the request structure to fetch all customer details
     When I hit an api
+      | method | endPoint  |
+      | GET    | customers |
     Then I verify all customer details in the response
+      | Joda Consulting Inc |
+
+  Scenario: Verify customer information in get by id api
+    Given I set up the request structure to fetch all customer details
+    When I hit an api
+      | method | endPoint  | pathParam |
+      | GET    | customers | 6         |
--- src/test/java/stepdefinitions/CustomerStepDefinition.java
+++ src/test/java/stepdefinitions/CustomerStepDefinition.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -7,6 +8,9 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
+
+import java.util.List;
+import java.util.Map;
 
 public class CustomerStepDefinition {
 
@@ -30,18 +34,49 @@ public class CustomerStepDefinition {
     }
 
     @When("I hit an api")
-    public void iHitAnApi() {
+    public void iHitAnApi(DataTable table) {
         //hit an api and get the response
-         response= requestSpecification.get("/customers");
+        Map<String,String> data = table.asMaps().get(0);
+        if(data.get("method").equals("GET")){
+            String pathParam="";
+            if(data.get("pathParam") != null){
+                pathParam = data.get("pathParam");
+            }
+            response= requestSpecification.get("/"+ data.get("endPoint")+"/"+pathParam);
+        }
          response.prettyPrint(); // print the response in pretty format
 
     }
 
     @Then("I verify all customer details in the response")
-    public void iVerifyAllCustomerDetailsIntTheResponse() {
+    public void iVerifyAllCustomerDetailsIntTheResponse(String expectedCustomerName) {
 
         int actualStatusCode =    response.getStatusCode();
         Assert.assertEquals(200,actualStatusCode);
+   /*     String firstCustName= response.jsonPath().getString("items[0].name");
+        System.out.println(firstCustName);
+        Assert.assertEquals("Big Bang Company", firstCustName);
+       */
+
+        List<Map<String,Object>> itemsList = response.jsonPath().getList("items");
+        System.out.println(itemsList.size() ); //print the size of the item list
+        System.out.println(itemsList);
+        boolean actualResult = false;
+        boolean actualArchived = false;
+        for(Map<String,Object> customerDetails :itemsList ){
+            // get the customer name from the response
+            String customerName = customerDetails.get("name").toString();
+            //check if the expected customer name is present in the response
+            if (customerName.equals(expectedCustomerName)){
+                //get the archived value for Joda Consulting Inc customer
+                actualArchived = (boolean)customerDetails.get("archived");
+                System.out.println("Successfully able to verify customer name in the response");
+                actualResult= true;
+                break;
+            }
+        }
+        Assert.assertTrue(actualResult); //  assert the line if actualResult variable value if not true
+        Assert.assertTrue(actualArchived); // asserting the archived value in Joda Consulting Inc customer
 
     }
 }
